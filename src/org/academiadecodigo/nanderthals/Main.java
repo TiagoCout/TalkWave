53a2fdc2705fce9b92650478a4bb02533de4126c
TiagoCout <tiagolcout@gmail.com>
2024-02-10 10:17:51 +0000

    Committing changes before addressing line endings warning.

---
+++ src/org/academiadecodigo/nanderthals/Main.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.nanderthals;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First line of TalkWave");
+    }
+}
---
+++ src/org/academiadecodigo/nanderthals/Server.java
@@ -0,0 +1,4 @@
+package org.academiadecodigo.nanderthals;
+
+public class Server {
+}
